31d9d1df798d7cf01ce84ff198d525ec4387a2b3
Vivek Pandey <vivek.pandey@gmail.com>
2013-05-25 11:31:27 -0700

    CR Application type

---
+++ core/src/main/java/com/cloudbees/cloud_resource/types/CloudResourceApplication.java
@@ -0,0 +1,36 @@
+package com.cloudbees.cloud_resource.types;
+
+import org.codehaus.jackson.annotate.JsonProperty;
+
+/**
+ * Represents  PaaS application CR
+ *
+ * @author Vivek Pandey
+ */
+@CloudResourceType("https://types.cloudbees.com/paas/application")
+public interface CloudResourceApplication{
+
+    @JsonProperty("application")
+    public Application application();
+
+    public class Application{
+
+        private String url;
+
+        public Application(String url) {
+            this.url = url;
+        }
+
+        public Application() {
+        }
+
+        @JsonProperty("url")
+        public String getUrl(){
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+    }
+}
