9fa058b0cfd776837595cb7ea4f7c16a6455a2a5
Vivek Pandey <vivek.pandey@gmail.com>
2013-05-29 21:06:03 -0700

    CRP registry type

---
+++ core/src/main/java/com/cloudbees/cloud_resource/types/CloudResourceProviderRegistry.java
@@ -0,0 +1,12 @@
+package com.cloudbees.cloud_resource.types;
+
+/**
+ *
+ *  A Cloud Resource Provider that manages CRP registry
+ *
+ * @author Vivek Pandey
+ */
+@CloudResourceType("https://types.cloudbees.com/resource/provider/crp/registry")
+public interface CloudResourceProviderRegistry extends CloudResourceProvider{
+
+}
