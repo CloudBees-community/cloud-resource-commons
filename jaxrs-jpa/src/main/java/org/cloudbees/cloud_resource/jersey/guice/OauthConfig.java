e15fb57a19288c5eb0a781310a0cdd8b6a9137c3
Vivek Pandey <vivek.pandey@gmail.com>
2014-02-13 14:38:50 -0800

    OauthConfig is a concrete class and can be bound directly to guice injector

--- jaxrs-jpa/src/main/java/org/cloudbees/cloud_resource/jersey/guice/OauthConfig.java
+++ jaxrs-jpa/src/main/java/org/cloudbees/cloud_resource/jersey/guice/OauthConfig.java
@@ -1,10 +1,12 @@
 package org.cloudbees.cloud_resource.jersey.guice;
 
+import com.google.inject.Singleton;
+
 /**
  *
- * Guice injectable Configuration object for Cloud Resource Provider
+ * Guice injectable OAuth Configuration object
  *
- * A CRP implementation should bind it to actual implementation class.
+ * A CRP implementation should bind instance of it
  *
  * This example shows how you can bind a Github CRP config object to OauthConfig with JerseyServletModule
  *
@@ -18,7 +20,7 @@ package org.cloudbees.cloud_resource.jersey.guice;
  *
  *              @Override
  *              protected void configureServlets() {
- *                  bind(OauthConfig.class).to(GithubOauthConfig.class);
+ *                  bind(new OauthConfig(clientId,clientSecret));
  *              }
  *          }
  *      }
@@ -27,14 +29,29 @@ package org.cloudbees.cloud_resource.jersey.guice;
  *
  * @author Vivek Pandey
  */
-public abstract class OauthConfig {
+@Singleton
+public  class OauthConfig {
+
+    private final String clientId;
+
+    private final String clientSecret;
+
+    public OauthConfig(String clientId, String clientSecret) {
+        this.clientId = clientId;
+        this.clientSecret = clientSecret;
+    }
+
     /**
      * OAuth application clientId
      */
-    public abstract String getClientId();
+    public  String getClientId(){
+        return clientId;
+    }
 
     /**
      * OAuth application clientSecret
      */
-    public abstract String getClientSecret();
+    public String getClientSecret(){
+        return clientSecret;
+    }
 }
